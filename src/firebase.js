f112810d252ad38415e9d283f94b3879942207d4
Abhinob-Bora <abhinobbora6@gmail.com>
2024-06-17 16:50:49 +0530

    secure

--- src/firebase.js
+++ src/firebase.js
@@ -6,15 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCynCuPri9pbdRxX39c8KNBcrfQWZ4180I",
-  authDomain: "stylesenseai1.firebaseapp.com",
-  projectId: "stylesenseai1",
-  storageBucket: "stylesenseai1.appspot.com",
-  messagingSenderId: "282279592964",
-  appId: "1:282279592964:web:73498aaa80862492aaa151",
-  measurementId: "G-4B3F0MD9B0"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
